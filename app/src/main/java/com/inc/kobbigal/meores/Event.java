26319aa16b6d878af4795272ccd0921221a217b3
Kobbi Gal <kgwack@gmail.com>
2017-11-04 15:38:58 +0200

    UI work

---
+++ app/src/main/java/com/inc/kobbigal/meores/Event.java
@@ -0,0 +1,72 @@
+package com.inc.kobbigal.meores;
+
+import android.location.Location;
+
+import java.util.Calendar;
+
+/**
+ * Created by Kobbi.Gal on 04/11/2017.
+ */
+
+public class Event {
+
+    private int id;
+    private String name;
+    private String location;
+    private String datetime;
+    private int numberOfAttendees;
+    private int statusId;
+
+    public Event() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+    //private int totalCost;
+
+}
---
+++ app/src/main/java/com/inc/kobbigal/meores/EventAdapter.java
@@ -0,0 +1,74 @@
+package com.inc.kobbigal.meores;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kobbi.Gal on 04/11/2017.
+ */
+
+public class EventAdapter extends BaseAdapter implements View.OnClickListener {
+
+    private List<Event> eventList;
+    private Context context;
+
+    public EventAdapter(Context context, List<Event> eventList) {
+        this.eventList = eventList;
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return eventList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return eventList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        Event event = eventList.get(i);
+
+        if (view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.event_list_row, viewGroup, false);
+        }
+
+        View statusBar = view.findViewById(R.id.status_bar);
+        TextView eventTitle = view.findViewById(R.id.event_title);
+        TextView eventLocation = view.findViewById(R.id.event_location);
+        TextView eventTime = view.findViewById(R.id.event_time);
+        TextView eventDate = view.findViewById(R.id.event_date);
+        Button edit = view.findViewById(R.id.edit_event);
+
+        statusBar.setBackgroundColor();
+        eventTitle.setText(event.getName());
+        eventLocation.setText((CharSequence) event.getLocation());
+        eventTime.setText(event.getDatetime().getTime());
+    }
+}
